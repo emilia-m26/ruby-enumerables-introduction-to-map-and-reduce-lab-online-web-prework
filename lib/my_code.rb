e6f5f45060aa3b56603dc1f08ea8759a4e167185
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-25 01:53:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,30 @@
+def my_map(source_array)
+  counter = 0
+  map_array = []
+  while counter < source_array.length do 
+    map_array << yield(source_array[counter]) 
+   counter += 1 
+  end 
+  map_array
+end
+
 def map_to_negativize(source_array)
-  source_array.map {|n| n*-1}
+  my_map(source_array) {|n| n*-1}
 end
 
 def map_to_no_change(source_array)
   source_array
 end
 
+
+
+# my_map([1,2,3]) do |num|
+#   num * 2
+# end 
+
 def map_to_double(source_array)
-  source_array.map {|n| n*2}
+ # source_array.map {|n| n*2}
+  my_map(source_array){|n| n*2}
 end
 
 def map_to_square(source_array)
@@ -15,7 +32,13 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-    source_array.reduce(starting_point) { |sum, num| sum + num}
+#    source_array.reduce(starting_point) { |sum, num| sum + num}
+    
+    sum = 0 + starting_point
+    source_array.each do |number|
+      sum += number
+    end
+     sum 
 end
 
 def reduce_to_all_true(source_array)
@@ -25,5 +48,7 @@ end
 def reduce_to_any_true(source_array)
   source_array.any?
 end
+
+
 
 
